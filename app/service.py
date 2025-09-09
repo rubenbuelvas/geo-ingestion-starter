f97e51dbefe0f64b5eedf758b04398a0e848729b
rubenbuelvas <rcbuelvasv@gmail.com>
2025-09-08 19:48:30 -0500

    Remove schemas from service

--- app/service.py
+++ app/service.py
@@ -4,8 +4,6 @@ import models
 from datetime import datetime, timezone
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from app import schemas
-
 
 def create_feature(db: Session, name: str, lat: float, lon: float) -> uuid.UUID:
     feature_id: uuid.UUID = uuid.uuid4()
@@ -17,25 +15,25 @@ def create_feature(db: Session, name: str, lat: float, lon: float) -> uuid.UUID:
     db.session.execute(text(f"ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326)::geography"))
     db.add(feature)
     db.commit()
-    return schemas.CreateFeatureResponse(id=feature_id)
+    return feature_id
 
 def process_feature(db: Session, feature_id: str, buffer_m: int = 500) -> bool:
     feature_id_uuid = uuid.UUID(feature_id)
     feature = db.query(models.Feature).filter_by(id=feature_id_uuid).first()
     if not feature:
-        return schemas.ProcessFeatureResponse(processed=False)
+        return False
     feature.geom = f"ST_Buffer(feature.geom, {buffer_m})"
     feature.status = "done"
     feature.attempts += 1
     db.commit()
-    return schemas.ProcessFeatureResponse(processed=True)
+    return True
 
-def get_feature(db: Session, feature_id: str) -> models.Feature: # TODO move schema handling to api
+def get_feature(db: Session, feature_id: str) -> models.Feature:
     feature_id_uuid = uuid.UUID(feature_id)
     return db.query(models.Feature).filter_by(id=feature_id_uuid).first()
 
 # Distance to point or to any part within area or buffer?
-def features_near(db: Session, lat: float, lon: float, radius_m: int) -> list[schemas.FeaturesNearResponse]:
+def features_near(db: Session, lat: float, lon: float, radius_m: int) -> list[models.Feature]:
     point_wkt = f"SRID=4326;POINT({lon} {lat})"
     query = text("""
         SELECT id, name, status, geom, attempts, created_at, updated_at,
@@ -46,5 +44,4 @@ def features_near(db: Session, lat: float, lon: float, radius_m: int) -> list[sc
     """)
     results = db.execute(query, {"point": point_wkt, "radius": radius_m}).fetchall()
     return results
-    # TODO: select features within radius from features and footprints tables
-    raise NotImplementedError
+
